88d5b01c90efe9c4ae7e3bf80c7e9c9425943111
excing <courage.exc@gmail.com>
2020-11-10 18:18:17 +0800

    Parameter "email" was renamed to "to"

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import "github.com/gin-gonic/gin"
 
 func main() {
-	SetupMailCredentials("Enter your email: ", "Enter email's password: ")
+	SetupMailCredentials("Enter your email: ", "Enter your alias: ", "Enter email's password: ")
 	router := gin.Default()
 	v1 := router.Group("/v1")
 	v1.POST("/send/mail", inout(PostSendMail))
@@ -13,7 +13,7 @@ func main() {
 // PostSendMail send mail
 func PostSendMail(c *Context) error {
 	type Mail struct {
-		Email   string `schema:"email,required"`
+		To      string `schema:"to,required"`
 		Subject string `schema:"subject,required"`
 		Body    string `schema:"body,required"`
 	}
@@ -25,7 +25,7 @@ func PostSendMail(c *Context) error {
 		return c.BadRequest(err.Error())
 	}
 
-	if err = SendMail(mail.Email, mail.Subject, mail.Body); err != nil {
+	if err = SendMail(mail.To, mail.Subject, mail.Body); err != nil {
 		return c.BadRequest(err.Error())
 	}
 
